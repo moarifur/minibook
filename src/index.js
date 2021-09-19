c26cfb882f21485510b42d6ebc38b15b1331c132
arif <moarifur@gmail.com>
2021-09-19 18:56:17 +0600

    Mini Book Project with JS ans CSS - Static Site

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firstBook = {
+  title: "I love You to the Moon and Back",
+  author: "Amelia Hepworth",
+  imgLink: "https://tinyurl.com/4px79m2u",
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+const Book = () => {
+  return (
+    <article className="book">
+      <img src={firstBook.imgLink} width="280" height="200" />
+      <h1>{firstBook.title}</h1>
+      <h4>{firstBook.author.toUpperCase()}</h4>
+    </article>
+  );
+};
+ReactDom.render(<BookList />, document.getElementById("root"));
